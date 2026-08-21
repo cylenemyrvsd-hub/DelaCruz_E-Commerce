9c10d42f422d7f8f3b6b2b72c4bba1a47882e2c3
cylenemyrvsd-hub <cylenemyrvsd@gmail.com>
2026-08-21 23:10:13 +0800

    Delete side-menu.js

--- side-menu.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.menu-item').forEach(item => {
-  item.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelectorAll('.menu-item').forEach(i => i.classList.remove('active'));
-    this.classList.add('active');
-  });
-});
